f99a02cd12cc742732d52dd34f02ebf0569a6180
lilingjun <lilingjun01@inspur.com>
2023-12-06 16:37:44 +0800

    upload inference scripts for 2B on humaneval, update bash scripts for all models on humaneval and update .md files

--- examples/eval_humaneval_51B.sh
+++ examples/eval_humaneval_51B.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Runs the "Yuan-102B" parameter model inference
+# Runs the "Yuan-51B" parameter model inference
+
 
 GPUS_PER_NODE=8
 MASTER_ADDR=localhost
@@ -20,7 +21,7 @@ if [ "$TOP_K" == "" ]; then
     TOP_K=1
 fi
 if [ "$CASE_NAME" == "" ]; then
-    CASE_NAME=test-102B
+    CASE_NAME=test-51B
 fi
 
 TOKENIZER_MODEL_PATH=./tokenizer
@@ -37,14 +38,14 @@ GPT_ARGS="
     --micro-batch-size 1 \
     --tensor-model-parallel-size 8 \
     --pipeline-model-parallel-size 1 \
-    --num-layers 84 \
+    --num-layers 42 \
     --hidden-size 8192 \
     --use-lf-gate \
+    --use-flash-attn \
     --lf-conv2d-group 1 \
     --lf-conv2d-num-pad 0 \
     --position-embedding-type rope \
     --no-embedding-dropout \
-    --use-flash-attn \
     --flash-attn-drop 0.0 \
     --attention-dropout 0 \
     --fim-rate 0.0 \
@@ -85,3 +86,4 @@ torchrun $DISTRIBUTED_ARGS tasks/humaneval/eval_humaneval.py \
        --output_path $OUTPUT_PATH \
        --load $CHECKPOINT_PATH 2>&1 | tee ${LOG_PATH}/eval_${NODE_RANK}_${CASE_NAME}.log
 evaluate_functional_correctness -p datasets/HUMANEVAL/${DATASET}  ${OUTPUT_PATH}/samples.jsonl 2>&1 | tee ${OUTPUT_PATH}/result.txt
+
