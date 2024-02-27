4cdf1e0d0e373774751fff869c55e01e9cad9cf2
liuxin-ieisystem <50068070+liuxm117@users.noreply.github.com>
2024-02-27 09:38:36 +0800

    update training.py

--- megatron/training.py
+++ megatron/training.py
@@ -26,7 +26,7 @@ from megatron import print_rank_last
 from megatron.checkpointing import load_checkpoint
 from megatron.checkpointing import save_checkpoint
 from megatron.model import Float16Module
-from megatron.model import GPTModel
+from megatron.model import GPTModel, YuanModel
 from megatron.core.enums import ModelType
 from megatron.optimizer import get_megatron_optimizer
 from megatron.initialize import initialize_megatron
@@ -267,7 +267,7 @@ def get_model(model_provider_func, model_type=ModelType.encoder_or_decoder, wrap
 
     # Disallow training and inference with Transformer Engine
     # for non-GPT models
-    args.allow_transformer_engine = all([type(m) == GPTModel for m in model])
+    args.allow_transformer_engine = all([type(m) == GPTModel for m in model]) or all([type(m) == YuanModel for m in model])
     assert args.allow_transformer_engine or args.transformer_impl == 'local', \
         'Transformer Engine is only approved for GPT models'
 
