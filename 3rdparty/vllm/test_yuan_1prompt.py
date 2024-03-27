d84647cd0250bbe57c1e0915cb3531a9c5d0666d
root <root@node1.cluster.local>
2024-03-27 08:26:53 +0000

    vllm modify

--- 3rdparty/vllm/test_yuan_1prompt.py
+++ 3rdparty/vllm/test_yuan_1prompt.py
@@ -2,7 +2,7 @@ from vllm import LLM, SamplingParams
 import time
 import os
 
-prompts = ["大学生写简历有哪些思路建议和注意事项"]
+prompts = ["如果你是一个算法工程师，让你写一个大模型相关的规划，你应该怎么写？"]
 sampling_params = SamplingParams(max_tokens=300, temperature=1, top_p=0, top_k=1, min_p=0.0, length_penalty=1.0, repetition_penalty=1.0, stop="<eod>", )
 
 llm = LLM(model="/temp_data/LLM_test/Tensorrt-llm-yuan/yuan2B_Janus", trust_remote_code=True)
